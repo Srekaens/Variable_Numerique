5b975436643938b4aea430cc169c6a3194548fa9
Justin <justindidelot8@outlook.fr>
2024-08-12 11:42:08 +0200

    Première version de l'exercice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿
-Console.WriteLine("Hello, coucou!");
+Console.Write("Veuillez saisir votre premier nombre : ");
+double premierNombre = double.Parse(Console.ReadLine());
+Console.Write("Veuillez saisir votre premier nombre : ");
+double deuxiemeNombre = double.Parse(Console.ReadLine());
+
+double resultat = premierNombre + deuxiemeNombre;
+Console.WriteLine($"Le résultat de votre addition est de : {resultat} ");
+Console.WriteLine("Appuyez sur entrée pour quitter.");
+Console.ReadKey();
